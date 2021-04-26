de53a9451ac755372996c90e2ee90ebe04f670ad
Amine <med.bagui.1@gmail.com>
2021-04-26 18:50:57 +0000

    added more logic into rendering convo with id using useEffect

--- src/Components/AllConvos.js
+++ src/Components/AllConvos.js
@@ -1,23 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {useParams} from 'react-router-dom';
 
 
 
-  function AllConvos(){
-
+function AllConvos(){ 
   const [current,setCurrent] = useState({})
+  useEffect(() =>  {
+    if(id !== undefined){
+      axios.get(`/convos/${id}`).then(res => setCurrent(res.data));
+      console.log("made a post request")
+    }
+  },[])
+
+  
 
   const {id} = useParams();
-  // console.log(id);
+  console.log(id);  
   
-  axios.get(`/convos/${id}`).then(res => setCurrent(res.data));
+  
+
+
+  
   
 
 
   return(
     <div>
-      <h1>You will find all the conversations here {current.title} </h1>
+      <h1>You will find all the conversations here</h1>
     </div>
 
   )
