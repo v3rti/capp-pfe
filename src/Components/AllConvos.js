8b129e21641ef78dd36786799aba0b986d35043d
Amine <med.bagui.1@gmail.com>
2021-04-26 19:14:37 +0000

    started on the layout

--- src/Components/AllConvos.js
+++ src/Components/AllConvos.js
@@ -1,33 +1,44 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {useParams} from 'react-router-dom';
+import {CardMedia,Card, makeStyles} from '@material-ui/core'
 
 
+const useStyles = makeStyles({
+  fullCard : {
+    width: '1200px',
+    height: '700px',
+    backgroundColor: "grey",
+    margin: "auto",
+    
+  },
+  cardImage: {
+    width: "800px"
+  },
+ 
+
+})
 
 function AllConvos(){ 
-  const [current,setCurrent] = useState({})
+  
+  const [current,setCurrent] = useState({});
+
   useEffect(() =>  {
     if(id !== undefined){
-      axios.get(`/convos/${id}`).then(res => setCurrent(res.data));
-      console.log("made a post request")
-    }
+    axios.get(`/convos/${id}`).then(res => setCurrent(res.data));
+    console.log("made a post request")
+  }
   },[])
 
-  
-
+  const classes = useStyles();
   const {id} = useParams();
   console.log(id);  
-  
-  
-
-
-  
-  
-
-
+ 
   return(
-    <div>
-      <h1>You will find all the conversations here</h1>
+    <div className={classes.wrapper}>
+      <Card className={classes.fullCard}>
+        <CardMedia className={classes.cardImage} />
+      </Card>
     </div>
 
   )
