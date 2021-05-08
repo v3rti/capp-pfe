686b4f78778a18ca940886268205f632b15563ca
Amine <med.bagui.1@gmail.com>
2021-05-08 00:03:54 +0000

    rendering the right pages (redirecting to the login page / sign up page) based on both the jwt token existence/verification

--- src/Components/ContextTest/GlobalState.js
+++ src/Components/ContextTest/GlobalState.js
@@ -1,23 +1,33 @@
 import React,{useState, useEffect} from 'react';
 import MyContext from './MyContext';
 import axios from 'axios';
+import {useHistory, useLocation} from 'react-router-dom';
 
 function GlobalState(props){
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState({});
+  const history = useHistory();
+  const location = useLocation();
 
   useEffect(() => {
     setIsLoggedIn(localStorage.getItem('loginStatus') === "true");
     
+    axios.get('/status').then(res => {
+      setCurrentUser(res.data)
+    }).catch(error => {
+      setIsLoggedIn(false)
+      if(location.pathname !== "/signup"){
+        history.push('/login');
+      }
+    });
     
-    console.log(currentUser);
+    
     
   }, [])
 
   useEffect(() => {
     localStorage.setItem('loginStatus', isLoggedIn);
-    axios.get('/status').then(res => setCurrentUser(res.data))
   })
 
 
