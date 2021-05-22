cd9004c44bcfa235542a0d14565d954359e473d0
Amine <med.bagui.1@gmail.com>
2021-05-22 23:20:15 +0000

    users are now displayed only conversations they joined and not all conversations

--- src/Components/MoreConvos/Convo UI/RightSideBar.js
+++ src/Components/MoreConvos/Convo UI/RightSideBar.js
@@ -6,27 +6,40 @@ import {useHistory} from 'react-router-dom';
 
 function LeftSideBar(){
   
-  const {userConvos} = useContext(MyContext);
+  const {userConvos,currentUser} = useContext(MyContext);
   const classes = useStyles();
   const history = useHistory();
-
+  const [convosList,setConvosList] = useState([]);
   
 
 
   return(
     <div>
-      {userConvos ? userConvos.map(usrConvo => <Card onClick={() => {
-         history.push(`/conversations/${usrConvo.cuid}`)
-      }} className={classes.convoCard}>
-            <CardHeader avatar={
-              <Avatar className={classes.avatarColor}>
-                {usrConvo.title[0]}
-              </Avatar>
-            }
-            title={<Typography variant="body2">{usrConvo.title}</Typography>}
-            subheader={<Typography variant="body2" color="textSecondary" noWrap>{usrConvo.description} </Typography>}
-            />
-        </Card>) : null}
+      {/* {console.log(userConvos.map(conv => conv.users_joined.map(usr => {
+        if(usr.username === "medbagui1"){
+          return conv;
+        }
+      })))} */}
+
+      {userConvos ? userConvos.map(usrConvo => {
+      return usrConvo.users_joined.map(usr => {
+      if(usr.username === currentUser.username){
+        return <Card onClick={() => {
+          history.push(`/conversations/${usrConvo.cuid}`)
+       }} className={classes.convoCard}>
+             <CardHeader avatar={
+               <Avatar className={classes.avatarColor}>
+                 {usrConvo.title[0]}
+               </Avatar>
+             }
+             title={<Typography variant="body2">{usrConvo.title}</Typography>}
+             subheader={<Typography variant="body2" color="textSecondary" noWrap>{usrConvo.description} </Typography>}
+             />
+        </Card>
+      }
+      else{
+        
+      }})}) : null}
       
     </div>
   )
